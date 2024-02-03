c07fad81ce8567cae0fff91d2a731b3d94e8dc1e
Alex Tait <atait@ieee.org>
2024-02-03 14:55:24 -0500

    Populated files

---
+++ conf.py
@@ -0,0 +1,20 @@
+import sys
+import os
+# import sphinx_rtd_theme
+
+extensions = [
+]
+
+master_doc = 'index'
+
+# General information about the project.
+# project = u'KiCad Python API'
+# copyright = u'2015, KiCad Community'
+
+# The short X.Y version.
+# version = '0.4'
+# The full version, including alpha/beta/rc tags.
+# release = '0.4'
+
+# Output file base name for HTML help builder.
+# htmlhelp_basename = 'KiCadPythonAPIdoc'
---
+++ index.rst
@@ -0,0 +1,9 @@
+Kicad-python is now called kigadgets
+=======================================
+
+Redirecting to `kigadgets.readthedocs.io <https://kigadgets.readthedocs.io/en/latest/index.html>`_...
+
+.. meta::
+   :http-equiv=Refresh: 0; url='https://kigadgets.readthedocs.io/en/latest/index.html'
+
+.. toctree::
